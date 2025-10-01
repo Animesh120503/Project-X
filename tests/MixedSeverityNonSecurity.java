5226822b849a2d05f39848a7b8abef6e6c68bb51
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2025-10-01 20:42:18 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -1,12 +1,24 @@
+package tests;
+
+import java.util.logging.Logger;
+
 public class MixedSeverityNonSecurity {
 
+    private static final Logger LOGGER = Logger.getLogger(MixedSeverityNonSecurity.class.getName());
+
     public void infiniteLoop() {
-        while (true) {
-            System.out.println("Looping forever...");
+        int counter = 0; // Example end condition
+        while (counter < 100) { // Add an end condition to the loop
+            LOGGER.info("Looping...");
+            counter++;
         }
     }
 
     public void riskyAccess(String str) {
-        System.out.println(str.length());
+        if (str != null) { // Add null check to avoid potential NullPointerException
+            LOGGER.info("String length: " + str.length());
+        } else {
+            LOGGER.warning("Provided string is null.");
+        }
     }
 }
