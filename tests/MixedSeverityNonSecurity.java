9e44185e6d6830b539efdc0e7e5d4ba83b96d37f
Animesh Pradhan <animesh_pradhan@epam.com>
2025-10-01 20:30:05 +0530

    Add Issue in a File

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -1,24 +1,12 @@
-import java.util.logging.Logger;
-
 public class MixedSeverityNonSecurity {
 
-    private static final Logger LOGGER = Logger.getLogger(MixedSeverityNonSecurity.class.getName());
-
     public void infiniteLoop() {
-        // Fix for Blocker issue (java:S2189): Add an end condition to the loop
-        int counter = 0; // Example condition variable
-        while (counter < 10) { // Add a proper end condition
-            LOGGER.info("Looping... iteration: " + counter);
-            counter++;
+        while (true) {
+            System.out.println("Looping forever...");
         }
     }
 
     public void riskyAccess(String str) {
-        // Fix for Major issue (java:S106): Replace System.out with a logger
-        if (str != null) {
-            LOGGER.info("String length: " + str.length());
-        } else {
-            LOGGER.warning("Provided string is null.");
-        }
+        System.out.println(str.length());
     }
 }
