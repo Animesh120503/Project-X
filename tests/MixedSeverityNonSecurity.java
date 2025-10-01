7d7929ad05df0eb737a317205331f21878df18af
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2025-10-01 16:18:42 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -1,12 +1,24 @@
+import java.util.logging.Logger;
+
 public class MixedSeverityNonSecurity {
 
+    private static final Logger LOGGER = Logger.getLogger(MixedSeverityNonSecurity.class.getName());
+
     public void infiniteLoop() {
-        while (true) {
-            System.out.println("Looping forever...");
+        // Fix for Blocker issue (java:S2189): Add an end condition to the loop
+        int counter = 0; // Example condition variable
+        while (counter < 10) { // Add a proper end condition
+            LOGGER.info("Looping... iteration: " + counter);
+            counter++;
         }
     }
 
     public void riskyAccess(String str) {
-        System.out.println(str.length());
+        // Fix for Major issue (java:S106): Replace System.out with a logger
+        if (str != null) {
+            LOGGER.info("String length: " + str.length());
+        } else {
+            LOGGER.warning("Provided string is null.");
+        }
     }
 }
