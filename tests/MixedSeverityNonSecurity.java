bba1bb798e98b79208f4cc2f932e7d3e389620c6
Animesh Pradhan <animesh_pradhan@epam.com>
2025-10-02 21:53:01 +0530

    Added bugs to file

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -2,24 +2,22 @@ package wrongPackage;
 
 public class MixedSeverityNonSecurity {
 
+    // Infinite loop (bug / code smell)
     public void infiniteLoop() {
-        int counter = 0;
-        while (counter < 10) { // finite loop to avoid security vulnerability
-            String msg = "Looping iteration " + counter;
+        while (true) {
+            String msg = "Looping forever...";
             System.out.println(msg); // code smell: use of System.out
-            counter++;
         }
     }
 
+    // Risky access (bug)
     public void riskyAccess(String str) {
-        if (str != null) {
-            System.out.println("String length: " + str.length()); // code smell: System.out
-        } else {
-            System.out.println("String is null"); // bug: risky behavior without proper exception
-        }
+        int length = str.length(); // will throw NullPointerException if str is null
+        System.out.println("String length: " + length); // code smell: System.out
     }
 
-    public void complexLogic(int x) {
+    // Cognitive complexity example
+    public int complexFunction(int x) {
         int result = 0;
         if (x > 0) {
             if (x % 2 == 0) {
@@ -36,17 +34,27 @@ public class MixedSeverityNonSecurity {
                 result -= 1;
             }
         }
-        System.out.println("Result: " + result); // code smell: System.out
+        return result;
     }
 
+    // Nested loops for cognitive complexity
     public void nestedLoops(int n) {
-        for (int i = 0; i < n; i++) { // cognitive complexity test
+        for (int i = 0; i < n; i++) { // code smell: nested loops
             for (int j = 0; j < n; j++) {
                 if ((i + j) % 2 == 0) {
-                    System.out.println("Even sum: " + (i + j)); // code smell
+                    System.out.println("Even sum: " + (i + j)); // code smell: System.out
                 } else {
-                    System.out.println("Odd sum: " + (i + j)); // code smell
+                    System.out.println("Odd sum: " + (i + j)); // code smell: System.out
                 }
+            }
+        }
+    }
+
+    // Redundant conditional (code smell)
+    public void redundantCheck(int num) {
+        if (num > 0) {
+            if (num > 0) {
+                System.out.println("Number is positive"); // code smell: redundant condition
             }
         }
     }
