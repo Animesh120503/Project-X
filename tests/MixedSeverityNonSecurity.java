b76178b2cc9e46d3844abee8ce8537ef8ff678f7
Animesh Pradhan <animesh_pradhan@epam.com>
2025-10-02 20:41:08 +0530

    Added issues to java file

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -1,45 +1,32 @@
-package tests;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+package wrongPackage;
 
 public class MixedSeverityNonSecurity {
 
-    // Logger instance for logging messages
-    private static final Logger logger = LoggerFactory.getLogger(MixedSeverityNonSecurity.class);
-
     public void infiniteLoop() {
-        // Adding an end condition to the loop to resolve the BLOCKER issue
-        int counter = 0;
-        while (counter < 10) { // Example end condition: loop runs 10 times
-            String msg = "Looping iteration: " + counter;
-            logger.info(msg); // Replacing System.out with logger
-            counter++;
+        while (true) {
+            String msg = "Looping forever...";
+            System.out.println(msg);
         }
     }
 
     public void riskyAccess(String str) {
-        // Replacing System.out with logger
         int length = str.length();
-        logger.info("String length: {}", length);
+        System.out.println("String length: " + length);
     }
 
     private static final String DB_PASSWORD = "PASSWORD_TEST_ONLY";
 
     private static final String AWS_ACCESS_KEY = "AKIA_TEST_ACCESS_KEY_123456";
-    // Removed unused AWS_SECRET_KEY field to resolve the MAJOR issue
-    // private static final String AWS_SECRET_KEY = "SECRET_TEST_KEY_ABCDEF";
+    private static final String AWS_SECRET_KEY = "SECRET_TEST_KEY_ABCDEF";
 
     public void connectToDb() {
-        // Replacing System.out with logger
         String jdbcUrl = "jdbc:mysql://localhost:3306/testdb?user=root&password=" + DB_PASSWORD;
-        logger.info("Connecting with URL: {}", jdbcUrl);
+        System.out.println("Connecting with URL: " + jdbcUrl);
     }
 
     public void callExternalApi() {
-        // Replacing System.out with logger
         String endpoint = "https://api.example.com/data";
         String apiKey = AWS_ACCESS_KEY;
-        logger.info("Calling {} with key: {}", endpoint, apiKey);
+        System.out.println("Calling " + endpoint + " with key: " + apiKey);
     }
 }
