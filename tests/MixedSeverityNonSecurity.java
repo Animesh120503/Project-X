1ba029ed4b9e53daed4781b5032b13adeace8b15
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2025-10-02 20:01:38 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -1,32 +1,40 @@
-package wrongPackage;
+package tests; // Fixed package name to match the file path
+
+import java.util.logging.Logger;
 
 public class MixedSeverityNonSecurity {
 
+    private static final Logger LOGGER = Logger.getLogger(MixedSeverityNonSecurity.class.getName()); // Added logger
+
     public void infiniteLoop() {
-        while (true) {
-            String msg = "Looping forever...";
-            System.out.println(msg);
+        // Fixed infinite loop by adding an end condition
+        int counter = 0;
+        while (counter < 100) { // Example end condition
+            String msg = "Looping...";
+            LOGGER.info(msg); // Replaced System.out with logger
+            counter++;
         }
     }
 
     public void riskyAccess(String str) {
         int length = str.length();
-        System.out.println("String length: " + length);
+        LOGGER.info("String length: " + length); // Replaced System.out with logger
     }
 
     private static final String DB_PASSWORD = "PASSWORD_TEST_ONLY";
 
     private static final String AWS_ACCESS_KEY = "AKIA_TEST_ACCESS_KEY_123456";
-    private static final String AWS_SECRET_KEY = "SECRET_TEST_KEY_ABCDEF";
+    // Removed unused private field AWS_SECRET_KEY
+    // private static final String AWS_SECRET_KEY = "SECRET_TEST_KEY_ABCDEF";
 
     public void connectToDb() {
         String jdbcUrl = "jdbc:mysql://localhost:3306/testdb?user=root&password=" + DB_PASSWORD;
-        System.out.println("Connecting with URL: " + jdbcUrl);
+        LOGGER.info("Connecting with URL: " + jdbcUrl); // Replaced System.out with logger
     }
 
     public void callExternalApi() {
         String endpoint = "https://api.example.com/data";
         String apiKey = AWS_ACCESS_KEY;
-        System.out.println("Calling " + endpoint + " with key: " + apiKey);
+        LOGGER.info("Calling " + endpoint + " with key: " + apiKey); // Replaced System.out with logger
     }
 }
