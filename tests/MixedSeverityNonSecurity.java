04329305cb9ec801cbd39995ae84b744e63f1333
Animesh Pradhan <animesh_pradhan@epam.com>
2025-10-02 21:19:48 +0530

    Added code smell and bugs

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -3,30 +3,51 @@ package wrongPackage;
 public class MixedSeverityNonSecurity {
 
     public void infiniteLoop() {
-        while (true) {
-            String msg = "Looping forever...";
-            System.out.println(msg);
+        int counter = 0;
+        while (counter < 10) { // finite loop to avoid security vulnerability
+            String msg = "Looping iteration " + counter;
+            System.out.println(msg); // code smell: use of System.out
+            counter++;
         }
     }
 
     public void riskyAccess(String str) {
-        int length = str.length();
-        System.out.println("String length: " + length);
+        if (str != null) {
+            System.out.println("String length: " + str.length()); // code smell: System.out
+        } else {
+            System.out.println("String is null"); // bug: risky behavior without proper exception
+        }
     }
 
-    private static final String DB_PASSWORD = "PASSWORD_TEST_ONLY";
-
-    private static final String AWS_ACCESS_KEY = "AKIA_TEST_ACCESS_KEY_123456";
-    private static final String AWS_SECRET_KEY = "SECRET_TEST_KEY_ABCDEF";
-
-    public void connectToDb() {
-        String jdbcUrl = "jdbc:mysql://localhost:3306/testdb?user=root&password=" + DB_PASSWORD;
-        System.out.println("Connecting with URL: " + jdbcUrl);
+    public void complexLogic(int x) {
+        int result = 0;
+        if (x > 0) {
+            if (x % 2 == 0) {
+                result += 2;
+            } else {
+                result += 3;
+            }
+        } else {
+            if (x < -10) {
+                result -= 10;
+            } else if (x < -5) {
+                result -= 5;
+            } else {
+                result -= 1;
+            }
+        }
+        System.out.println("Result: " + result); // code smell: System.out
     }
 
-    public void callExternalApi() {
-        String endpoint = "https://api.example.com/data";
-        String apiKey = AWS_ACCESS_KEY;
-        System.out.println("Calling " + endpoint + " with key: " + apiKey);
+    public void nestedLoops(int n) {
+        for (int i = 0; i < n; i++) { // cognitive complexity test
+            for (int j = 0; j < n; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.println("Even sum: " + (i + j)); // code smell
+                } else {
+                    System.out.println("Odd sum: " + (i + j)); // code smell
+                }
+            }
+        }
     }
 }
