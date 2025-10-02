f41b3fbc78a98fb5f7241fd195423e50f0964663
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2025-10-02 23:25:30 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -1,61 +1,63 @@
-package wrongPackage;
+// Corrected package name to match the file path and naming conventions
+package tests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MixedSeverityNonSecurity {
 
-    // Infinite loop (bug / code smell)
-    public void infiniteLoop() {
-        while (true) {
-            String msg = "Looping forever...";
-            System.out.println(msg); // code smell: use of System.out
+    // Logger instance to replace System.out
+    private static final Logger logger = LoggerFactory.getLogger(MixedSeverityNonSecurity.class);
+
+    // Fixed infinite loop by adding an end condition
+    public void finiteLoop() {
+        int counter = 0; // Added a counter to control the loop
+        while (counter < 10) { // End condition to prevent infinite loop
+            String msg = "Looping... iteration: " + counter;
+            logger.info(msg); // Replaced System.out with logger
+            counter++; // Increment counter to eventually exit the loop
         }
     }
 
-    // Risky access (bug)
-    public void riskyAccess(String str) {
-        int length = str.length(); // will throw NullPointerException if str is null
-        System.out.println("String length: " + length); // code smell: System.out
+    // Fixed risky access by adding null check
+    public void safeAccess(String str) {
+        if (str != null) { // Added null check to prevent NullPointerException
+            int length = str.length();
+            logger.info("String length: {}", length); // Replaced System.out with logger
+        } else {
+            logger.warn("Provided string is null"); // Log a warning if str is null
+        }
     }
 
-    // Cognitive complexity example
-    public int complexFunction(int x) {
+    // Simplified cognitive complexity in the function
+    public int simplifiedFunction(int x) {
         int result = 0;
         if (x > 0) {
-            if (x % 2 == 0) {
-                result += 2;
-            } else {
-                result += 3;
-            }
+            result += (x % 2 == 0) ? 2 : 3; // Combined nested if-else into a single line
+        } else if (x < -10) {
+            result -= 10;
+        } else if (x < -5) {
+            result -= 5;
         } else {
-            if (x < -10) {
-                result -= 10;
-            } else if (x < -5) {
-                result -= 5;
-            } else {
-                result -= 1;
-            }
+            result -= 1;
         }
         return result;
     }
 
-    // Nested loops for cognitive complexity
-    public void nestedLoops(int n) {
-        for (int i = 0; i < n; i++) { // code smell: nested loops
+    // Refactored nested loops to reduce cognitive complexity
+    public void processSums(int n) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if ((i + j) % 2 == 0) {
-                    System.out.println("Even sum: " + (i + j)); // code smell: System.out
-                } else {
-                    System.out.println("Odd sum: " + (i + j)); // code smell: System.out
-                }
+                String sumType = ((i + j) % 2 == 0) ? "Even" : "Odd";
+                logger.info("{} sum: {}", sumType, (i + j)); // Replaced System.out with logger
             }
         }
     }
 
-    // Redundant conditional (code smell)
-    public void redundantCheck(int num) {
-        if (num > 0) {
-            if (num > 0) {
-                System.out.println("Number is positive"); // code smell: redundant condition
-            }
+    // Merged redundant conditional statements
+    public void checkNumber(int num) {
+        if (num > 0) { // Removed redundant nested condition
+            logger.info("Number is positive"); // Replaced System.out with logger
         }
     }
 }
