ed798467d5e1ee30f6fe8af35b8846ed00d97ccd
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2026-02-15 14:56:14 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -41,9 +41,9 @@ for (int i = 0; i < 10; i++) {
         for (int i = 0; i < n; i++) { // code smell: nested loops
             for (int j = 0; j < n; j++) {
                 if ((i + j) % 2 == 0) {
-                    System.out.println("Even sum: " + (i + j)); // code smell: System.out
+                    java.util.logging.Logger.getLogger(MixedSeverityNonSecurity.class.getName()).info("Even sum: " + (i + j));
                 } else {
-                    System.out.println("Odd sum: " + (i + j)); // code smell: System.out
+                    java.util.logging.Logger.getLogger(MixedSeverityNonSecurity.class.getName()).info("Odd sum: " + (i + j));
                 }
             }
         }
