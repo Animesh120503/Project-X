835ef1f10ee057a60d765b73f4b79e5f3b94268d
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2026-02-15 14:48:31 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -53,11 +53,8 @@ for (int j = 0; j < n; j++) {
     }
 
     // Redundant conditional (code smell)
-    public void redundantCheck(int num) {
-        if (num > 0) {
-            if (num > 0) {
-                System.out.println("Number is positive"); // code smell: redundant condition
-            }
-        }
+public void redundantCheck(int num) {
+    if (num > 0) {
+        System.out.println("Number is positive"); // code smell: redundant condition
     }
-}
+}}
