44080dcb74514090f37f069e1b05d4c9e0b5f803
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2026-02-15 13:14:22 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -53,8 +53,6 @@ public void nestedLoops(int n) {
 public void redundantCheck(int num) {
     java.util.logging.Logger logger = java.util.logging.Logger.getLogger(MixedSeverityNonSecurity.class.getName());
     if (num > 0) {
-        if (num > 0) {
-            logger.info("Number is positive"); // code smell: redundant condition
-        }
+        logger.info("Number is positive");
     }
 }}
