ed5122180afbb3c601177ed7941abdb9d3de8cb2
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2026-02-16 11:30:38 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -4,9 +4,11 @@ public class MixedSeverityNonSecurity {
 
     // Infinite loop (bug / code smell)
     public void infiniteLoop() {
-        while (true) {
+        int i = 0;
+        while (i < 10) {
             String msg = "Looping forever...";
             System.out.println(msg); // code smell: use of System.out
+            i++;
         }
     }
 
