ff891d3cda694f13c2f84d118f92df03eaddccc6
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2026-02-20 09:43:46 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -15,7 +15,7 @@ public class MixedSeverityNonSecurity {
     // Risky access (bug)
     public void riskyAccess(String str) {
         int length = str.length(); // will throw NullPointerException if str is null
-        System.out.println("String length: " + length); // code smell: System.out
+        java.util.logging.Logger.getLogger(MixedSeverityNonSecurity.class.getName()).info("String length: " + length);
     }
 
     // Cognitive complexity example
