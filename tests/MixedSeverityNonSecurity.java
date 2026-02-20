42f8d000c687cda9155533bf3b2ee72bb82bdd08
Animesh120503 <144404501+Animesh120503@users.noreply.github.com>
2026-02-20 09:44:09 +0530

    Update tests/MixedSeverityNonSecurity.java

--- tests/MixedSeverityNonSecurity.java
+++ tests/MixedSeverityNonSecurity.java
@@ -6,9 +6,10 @@ public class MixedSeverityNonSecurity {
     public void infiniteLoop() {
         // Add a termination condition to avoid an infinite loop.
         final int MAX_ITERATIONS = 1000;
+        final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(MixedSeverityNonSecurity.class.getName());
         for (int i = 0; i < MAX_ITERATIONS; i++) {
             String msg = "Looping forever...";
-            System.out.println(msg); // code smell: use of System.out
+            logger.info(msg);
         }
     }
 
